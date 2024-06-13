2945dceb7c14f4a870ddf0af076503d7d3f4cbff
Gowri-mamidi <mamidigowri96@gmail.com>
2024-06-13 11:01:55 +0530

    new change

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,9 +3,9 @@ import {Link} from 'react-router-dom'
 export default function Navigation() {
   return (
     <div>
-      {/* <Link to="/">Home</Link> */}
-       {/* <Link to="/about">About</Link>  */}
-      {/* <Link to="/contact">Contact</Link> */}
+      <Link to="/">Home</Link>
+       <Link to="/about">About</Link>  
+      <Link to="/contact">Contact</Link>
     </div>
   )
 }
